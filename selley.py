e4bb540fa592d1d75a27082b0e8fe2fcd43ac8c8
Arshia <arshiamotaghian@yahoo.com>
2024-07-31 13:00:49 +0330

    new sell

--- selley.py
+++ selley.py
@@ -1,14 +1,17 @@
-from product import product
-from product import product_manager
+from product import Product
+from product import ProductManager
+
+
 class Seller:
-  def init(self):
-    self._product = product()
-    self.product_manger = product_manager()
-  def add_product(self,name,price, category):
-    self._product.price = price
-    self._product.category = category
-    self._product.product_name = name 
-    self.product_manger.
-  def increase_quantity(self,tmp):
-    self._product.quantity += tmp
-  
+    def init(self):
+        self._product = Product()
+        self.product_manger = ProductManager()
+
+    def add_product(self, name, price, category):
+        self._product.price = price
+        self._product.category = category
+        self._product.product_name = name
+        self.product_manger.product_list.append(self)
+
+    def increase_quantity(self, tmp):
+        self._product.quantity += tmp
