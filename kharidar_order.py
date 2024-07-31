6444f7bc59652eee2b511cc50b64be155cbbe163
Arshia <arshiamotaghian@yahoo.com>
2024-07-31 14:16:55 +0330

    new

--- kharidar_order.py
+++ kharidar_order.py
@@ -1,22 +1,22 @@
-class Kharidar :
-
-    def __init__(self , name , password ) -> None:
-        self.name = name
-        self.password = password
+from user import User
 
 
-class Order :
+class Kharidar(User):
+    def __init__(self, name, password, role) -> None:
+        super().__init__(name, password, role)
 
-    def __init__(self , kharidar : Kharidar) -> None:
+class Order:
+
+    def __init__(self, kharidar: Kharidar) -> None:
         self.kharidar = kharidar
         self.products = []
 
-    def add_product (self , product : Product) :
+    def add_product(self, product: Product):
         self.products.append(product)
 
-    def order_list (self) : 
-        for product in self.products : 
-            print(product.get_name(), end= " " )
-            print(product.get_category() , end = " ")
-            print(product.get_city(), end =" " )
+    def order_list(self):
+        for product in self.products:
+            print(product.get_name(), end=" ")
+            print(product.get_category(), end=" ")
+            print(product.get_city(), end=" ")
             print()
--- selley.py
+++ selley.py
@@ -1,22 +1,18 @@
 from product import Product
 from product import ProductManager
+from user import User
 
 
-class Seller:
-    def __init__(self, product_manger: ProductManager, name, password):
+class Seller(User):
+    def __init__(self, product_manger: ProductManager):
         self.__product_manger = product_manger
         self.__products = []
-        self.__name = name
-        self.__password = password
 
     def get_name(self):
         return self.__name
 
     def get_password(self):
         return self.__password
-
-    def seller_product_managet(self):
-        return self.__product_manger
 
     def add_product(self, name, price, category):
         self._product.price = price
@@ -35,3 +31,11 @@ class Seller:
     def increase_quantity(self, tmp, product_name):
         needed_product = self.find_product(product_name)
         needed_product.quantity += tmp
+
+    def get_list_product(self):
+        product: Product
+        for product in self.__products:
+            print(product.get_product_name(), end=" ")
+            print(product.get_category(), end=" ")
+            print(product.get_origin(), end=" ")
+            print()
