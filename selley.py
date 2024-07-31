95b91c40a5ca43f7d8ebb5df7ceb5e67a0b02269
taha tahmasbypour <tahatahmasbypour@tahas-MacBook-Pro.local>
2024-07-31 12:54:39 +0330

    new

--- selley.py
+++ selley.py
@@ -1,13 +1,14 @@
+from product import product
+from product import product_manager
 class Seller:
   def init(self):
-    self. Product_Manager = ProductManager()
-    self.Product = Product
-    pass
-  def add_product(self):
-    pass
-  def increase_quantity(self):
-    pass
-  def get_order_list(self):
-    pass
+    self._product = product()
+    self.product_manger = product_manager()
+  def add_product(self,name,price, category):
+    self._product.price = price
+    self._product.category = category
+    self._product.product_name = name 
+    self.product_manger.
+  def increase_quantity(self,tmp):
+    self._product.quantity += tmp
   
-  pass
