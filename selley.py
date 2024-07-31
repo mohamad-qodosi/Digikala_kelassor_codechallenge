8693d80d9c9883ac81eb1ec4ea0d776a6c10998f
taha tahmasbypour <tahatahmasbypour@tahas-MacBook-Pro.local>
2024-07-31 12:31:04 +0330

    newseller

---
+++ selley.py
@@ -0,0 +1,13 @@
+class Seller:
+  def init(self):
+    self. Product_Manager = ProductManager()
+    self.Product = Product
+    pass
+  def add_product(self):
+    pass
+  def increase_quantity(self):
+    pass
+  def get_order_list(self):
+    pass
+  
+  pass
