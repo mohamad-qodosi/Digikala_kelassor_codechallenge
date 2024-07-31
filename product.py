c56bb758d4da7308f2fe14c226974f532ccf5aaf
Alireza Shams <alerzs1023@gmail.com>
2024-07-31 12:42:46 +0330

    add_product_1

--- product.py
+++ product.py
@@ -1,4 +1,4 @@
-class product:
+class Product:
     def __init__(self , category , product_name,  procceing_time, quantiy , price) -> None:
         self.category = category
         self.product_name = product_name
@@ -10,18 +10,31 @@ class product:
     def set_origin(self,origin):
         self.origin = origin
 
-class product_manager:
+class ProductManager:
     def __init__(self) -> None:
         self.product_list = []
 
     def search_product(self ,product_name):
-        pass
+        for product in self.product_list:
+            if product.product_name == product_name:
+                return product
+        return False
+
 
     def search_category(self , category):
-        pass
+        reserve =[]
+        for product in self.product_list:
+            if product.category == category:
+                reserve.append(product)
+        return reserve
 
     def search_by_price(self , price_range):
-        pass
+        show = []
+        for product in self.product_list:
+            if price_range[0] < product.price < price_range[1]:
+                show.append(product)
+        return show
 
     def add_product(self , product_obj):
-        pass
+        self.product_list.append(product_obj)
+
